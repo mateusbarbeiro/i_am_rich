ac8060822809040e86802b12d0ac3155ebb2ccad
mateus barbeiro <mateus_garcia01@hotmail.com>
2021-08-19 10:45:47 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(
+        MaterialApp(
+            home: Scaffold(
+                backgroundColor: Colors.blueGrey,
+                appBar: AppBar(
+                    title: const Text('Eu Sou Rico'),
+                    backgroundColor: Colors.blueGrey[900],
+                ),
+                body: const Center(
+                    child: Image(
+                        image: NetworkImage(
+                            'https://cdn.shopify.com/s/files/1/0018/5427/5661/files/flawless-diamond.png?9896225357560635367',
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    );
+}
